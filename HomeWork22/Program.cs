fb67c15ae99d71c509ccc3321d0060a04d316a75
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-12-15 00:36:41 +0300

    Добавьте файлы проекта.

---
+++ HomeWork22/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork22
+{
+    //Сформировать массив случайных целых чисел(размер задается пользователем). 
+    //Вычислить сумму чисел массива и максимальное число в массиве.
+    //Реализовать решение  задачи с  использованием механизма  задач продолжения.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите размер массива и диапазон случайных значений для его заполнения:");
+                int array_n = ReadPosValue("Размер массива:");
+                int randomMin = ReadValue("Минимальное значение диапазона:");
+                int randomMax = ReadValue("Максимальное значение диапазона:");
+                RandomArray array = new RandomArray(array_n, randomMin, randomMax);
+                array.Print();
+                object _array = array;
+                Console.WriteLine();
+                Action<object> Action_task1 = new Action<object>(array.FindSumm);
+                Task task1 = new Task(Action_task1, _array);
+                Action<Task, object> Action_task2 = new Action<Task, object>(array.FindMax);
+                Task task2 = task1.ContinueWith(Action_task2, _array);
+                task1.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Метод main завершен");
+            Console.ReadKey();
+        }
+        static int ReadPosValue(string text)   //метод проверяющий корректность ввода данных
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine(text);
+                if (Int32.TryParse(Console.ReadLine(), out value) && value > 0)
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Ввод некорректен");
+                }
+            }
+        }
+        static int ReadValue(string text)   //метод проверяющий корректность ввода данных
+        {
+            int value;
+            while (true)
+            {
+                Console.WriteLine(text);
+                if (Int32.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Ввод некорректен");
+                }
+            }
+        }
+    }
+   
+}
---
+++ HomeWork22/RandomArray.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork22
+{
+    class RandomArray
+    {
+        private int[] Array { get; set; }       //массив случайных чисел
+        private int Array_N { get; set; }       //длина массива
+        private int RandomMin { get; set; }     //начало диапазона случайных чисел
+        private int RandomMax { get; set; }     //конец диапазона случайных чисел
+        private int Summ { get; set; }          //сумма всех чисел
+        private int Max { get; set; }           //максимальное число в массиве
+
+        public RandomArray(int array_n, int randomMin, int randomMax)
+        {
+            RandomMin = randomMin;
+            RandomMax = randomMax;
+            Array_N = array_n;
+            Array = RandomFill();
+        }
+        private int[] RandomFill()          //метод заполняющий массив случайными числами
+        {
+            int[] array = new int[Array_N];
+            Random random = new Random();
+            for (int i = 0; i < Array_N; i++)
+            {
+                array[i] = random.Next(RandomMin, RandomMax);
+                Thread.Sleep(10);
+            }
+            return array;
+        }
+        public void Print()          //метод выводящий на консоль массив случайных чисел
+        {
+            foreach (int i in Array)
+            {
+                Console.Write($"\t{i}");
+            }
+        }
+        public void FindSumm(object a)      //метод подсчитывающий сумму чисел в массиве
+        {
+            RandomArray random_array = (RandomArray)a;
+            int[] array = random_array.Array;
+            int S = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                S += array[i];
+            }
+            Summ = S;
+            Thread.Sleep(2000);
+            Console.WriteLine($"Сумма элементов массива: {Summ}");
+        }
+        public void FindSumm(Task task, object a)      //метод подсчитывающий сумму чисел в массиве с перегрузкой для задачи продолжения
+        {
+            RandomArray random_array = (RandomArray)a;
+            int[] array = random_array.Array;
+            int S = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                S += array[i];
+            }
+            Summ = S;
+            Thread.Sleep(2000);
+            Console.WriteLine($"Сумма элементов массива: {Summ}");
+        }
+        public void FindMax(Task task, object a)      //метод для нахождения наибольшего числа в массиве
+        {
+            RandomArray random_array = (RandomArray)a;
+            int[] array = random_array.Array;
+            Max = array.Max();
+            Thread.Sleep(2000);
+            Console.WriteLine($"Наибольший элемент массива: {Max}");
+        }
+        public void FindMax(object a)            //метод для нахождения наибольшего числа в массиве с перегрузкой для задачи продолжения
+        {
+            RandomArray random_array = (RandomArray)a;
+            int[] array = random_array.Array;
+            Max = array.Max();
+            Thread.Sleep(2000);
+            Console.WriteLine($"Наибольший элемент массива: {Max}");
+        }
+    }
+}
